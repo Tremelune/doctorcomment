1d5fd555e21be61353a61f91e9182c3f72d183a1
Tremelune <w@quieteyes.com>
2018-03-16 17:15:55 -0700

    Docs

--- src/main/java/com/quieteyes/doctorcomment/Application.java
+++ src/main/java/com/quieteyes/doctorcomment/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@ComponentScan("com.quieteyes.doctorcomment") // This...doesn't work, so this class needs to be in the root package.
 public class Application {
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
